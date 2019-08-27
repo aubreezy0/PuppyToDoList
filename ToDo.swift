e408c133d7ec05cace6e260bfa83d34579e5743a
aubreezy0 <aubreezy0@gmail.com>
2019-08-27 15:20:58 -0400

    Adds new swift file for class ToDo Properties are name = "" and important : boolean = false Sets identifier for segue to moveToAddToDo

---
+++ ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  A Puppy's To Do List
+//
+//  Created by Aubree Lawrence on 8/27/19.
+//  Copyright © 2019 Aubree Lawrence. All rights reserved.
+//
+
+import Foundation
+
+class ToDo {
+    var name = ""
+    var important = false
+}
