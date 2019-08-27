fd57ec9ad183221d79a636bfe74ff50010844b02
aubreezy0 <aubreezy0@gmail.com>
2019-08-27 15:58:19 -0400

    Adds Cocoa Touch Class file addToDoViewController.swift Sets View Controller custom class to addToDoViewController Removes commented swift notes

---
+++ A Puppy's To Do List/addToDoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  addToDoViewController.swift
+//  A Puppy's To Do List
+//
+//  Created by Aubree Lawrence on 8/27/19.
+//  Copyright © 2019 Aubree Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class addToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
